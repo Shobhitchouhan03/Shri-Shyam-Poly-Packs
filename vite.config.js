30009b5cfb81c25e064b29c3ebd294a0bbfa7b57
shobhit <chouhanshobhit03@gmail.com>
2026-08-15 09:44:23 +0530

    fix: use root base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Shri-Shyam-Poly-Packs/",
+  base: "/",
   plugins: [
     react(),
     tailwindcss(),
